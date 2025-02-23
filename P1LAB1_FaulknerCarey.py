b4599c62b908a29d501ee6f664deb04db3a2e638
CareyFaulkner <faulknec7881@student.faytechcc.edu>
2025-02-23 18:47:42 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_FaulknerCarey.py
@@ -0,0 +1,12 @@
+# Carey Faulkner
+# 23 Febuary 2025
+# P1LAB1
+# Python Lab enter name output.
+
+print('First Name:', end=' ')
+first_name = input()
+
+print('Last Name:', end=' ')
+last_name = input()
+
+print("Hello", first_name, last_name, "Welcome to CTI-110!")
